3041ffab43606a89e9fef13d203ae062c2940545
Dave Friedman <davemfriedman@gmail.com>
2021-12-11 19:33:31 -0500

    first variable in Flask

--- Week 9/lecture/app.py
+++ Week 9/lecture/app.py
@@ -1,10 +1,11 @@
 #cs50 2020 lecture 9 Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     # return "Hello, World!"
-    return render_template("index.html")
+    # return render_template("index.html")
+    return render_template("index.html", name=request.args.get("name", "world"))
