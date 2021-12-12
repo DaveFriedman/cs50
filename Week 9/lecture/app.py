f72c519fb8d307ffb799a03c67677470a172ed07
Dave Friedman <davemfriedman@gmail.com>
2021-12-11 19:23:56 -0500

    Hello, world! from html, structured Flask folders

---
+++ Week 9/lecture/app.py
@@ -0,0 +1,10 @@
+#cs50 2020 lecture 9 Flask
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return "Hello, World!"
+    return render_template("index.html")
